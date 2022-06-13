652827ee636811cfed343bd96b3965a701041080
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-06-13 19:41:42 -0300

    Classe Pessoa - Classe e métodos criados

---
+++ Geen_Exercicio3/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geen_Exercicio3
+{
+    class Pessoa
+    {
+        public String Nome { get; set; }
+
+        public void AdicionarFilho(string nome, Pessoa pessoa)
+        {
+
+        }
+
+        public void Conjugue(Pessoa pessoa)
+        {
+
+        }
+
+        public void ImprimirArvore(Pessoa pessoa)
+        {
+
+        }
+    }
+}
